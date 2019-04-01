96bef4ee36500ef923db2cf8ae304e097940876d
As-Webmaster <unknow-master@hotmail.com>
2019-04-01 16:17:16 -0400

    start proyect siceli

---
+++ dist/wp-content/themes/sicelistartpage/index.php
@@ -0,0 +1,26 @@
+<?php
+    include './header.php';
+    include './main.php';
+    include './aside.php';
+    include './footer.php';
+?>
+
+<!DOCTYPE html>
+<html lang='es'>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <script src="main.js"></script>    
+    <script src="main.js"></script>    
+    <script src="main.js"></script>    
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>S.I.L.E.C.I. | As-Code</title>
+</head>
+<body>
+    
+</body>
+</html>
